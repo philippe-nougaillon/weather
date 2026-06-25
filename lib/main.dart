66d4399f4404d936df7036f6bcf4f2490ebb38be
Philippe NOUGAILLON <philippe.nougaillon@gmail.com>
2026-06-25 17:51:24 +0200

    all code done but with errors

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,95 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-void main() {
-  runApp(const MyApp());
-}
+class Weather {
+  final String description;
+  final double temperature;
+  final String iconCode;
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+  Weather({required this.description, required this.temperature, required this.iconCode});
+
+  factory Weather.fromJson(Map<String, dynamic> json) {
+    return Weather(
+      description: json['weather'][0]['description'], 
+      temperature: json['weather']['temp'].toDouble(), 
+      iconCode: json['weather'][0]['icon']
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class WeatherScreen extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _WeatherScreenState createState() => _WeatherScreenState();
+
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _WeatherScreenState extends State<WeatherScreen> {
+  late Future<Weather> futureWeather;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  Future<Weather> fetchWeather() async {
+    final url = 'https://api.openweathermap.org/data/2.5/weather/q=Paris&units=metrics&appid=8026186ef6bf29fdb211a2cc3b2a6d2b';
+
+    final response = await http.get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+      //print ('JSON récupéré : ${response.body}');
+      return Weather.fromJson(json.decode(response.body));
+    } else {
+      throw Exception('Erreur chargement météo');
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    futureWeather = fetchWeather();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(backgroundColor: Colors.indigo, 
+          title: const Text("Météo Paris", style: TextStyle(color: Colors.white)),
+        )
+        body: Center(
+          child: FutureBuilder<Weather>(
+            future: futureWeather, 
+            builder: (BuildContext context, AsyncSnapshot<Weather> snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return CircularProgressIndicator();
+              } else if (snapshot.hasError) {
+                return Text('Erreur :${snapshot.error}');
+              } else if (snapshot.hasData) {
+                final weather = snapshot.data!;
+                final iconUrl = 'http://openweathermap.org/img/wn/${weather.iconCode}@2x.png';
+
+                return Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.network(iconUrl),
+                    SizedBox(height: 20),
+                    Text('Description: ${weather.description}'),
+                    Text('Température: ${weather.temperature}'),
+                  ],
+                );
+              } else {
+                return Text('Aucune données');
+              }
+            },
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
     );
   }
+
 }
+
+
+void main() {
+ runApp(WeatherScreen());
+ 
+ }
+
