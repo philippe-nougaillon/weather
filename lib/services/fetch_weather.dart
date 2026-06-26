1ccf85836fca640c76d2e9205692987a3f940f52
Philippe NOUGAILLON <philippe.nougaillon@gmail.com>
2026-06-26 12:18:46 +0200

    api_key hidden

--- lib/services/fetch_weather.dart
+++ lib/services/fetch_weather.dart
@@ -1,9 +1,10 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:weather/models/weather.dart';
+import 'package:weather/api_key.dart';
 
 Future<Weather> fetchWeather() async {
-  final url = 'https://api.openweathermap.org/data/2.5/weather?q=Paris,fr&units=metric&APPID=8026186ef6bf29fdb211a2cc3b2a6d2b';
+  final url = 'https://api.openweathermap.org/data/2.5/weather?q=Paris,fr&units=metric&APPID=${open_weather_api_key}';
   final response = await http.get(Uri.parse(url));
 
   if (response.statusCode == 200) {
